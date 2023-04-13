42649d73b04d09750c4224cc6a53d3adbd4efee7
Mohamed Anis Bousselmi <anys.bslm@gmail.com>
2023-04-13 01:01:48 +0100

    npm init --yes npm i express nodemon cors cookie-parser jsonwebtoken bcrypt
    npm install react-router-dom axios react-cookie react-toastify
    
    npm install mongoose

---
+++ public/src/pages/Login.jsx
@@ -0,0 +1,92 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { Link, useNavigate } from "react-router-dom";
+import { useCookies } from "react-cookie";
+import { ToastContainer, toast } from "react-toastify";
+import { Button } from '@mui/material';
+import Toolbar from '@mui/material/Toolbar';
+import AppBar from '@mui/material/AppBar';
+
+
+function Login() {
+  const [cookies] = useCookies([]);
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (cookies.jwt) {
+      navigate("/");
+    }
+  }, [cookies, navigate]);
+
+  const [values, setValues] = useState({ email: "", password: "" });
+  const generateError = (error) =>
+    toast.error(error, {
+      position: "bottom-right",
+    });
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const { data } = await axios.post(
+        "http://localhost:4000/login",
+        {
+          ...values,
+        },
+        { withCredentials: true }
+      );
+      if (data) {
+        if (data.errors) {
+          const { email, password } = data.errors;
+          if (email) generateError(email);
+          else if (password) generateError(password);
+        } else {
+          navigate("/");
+        }
+      }
+    } catch (ex) {
+      console.log(ex);
+    }
+  };
+  return (
+    <>
+    <AppBar position="static">
+          <Toolbar sx={{ justifyContent: "right" }} className='toolbar'>
+              <a href="/register"><Button color="inherit">Sign Up</Button></a>
+              <a href="/Login"><Button color="inherit">Login</Button></a>
+          </Toolbar>
+      </AppBar>
+    <div className="App-header">Login to your account</div>
+      <p>Please fill out this form with the required informations</p>
+
+      <form onSubmit={(e) => handleSubmit(e)}>
+        <div>
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            name="email"
+            placeholder="Email"
+            onChange={(e) =>
+              setValues({ ...values, [e.target.name]: e.target.value })
+            }
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            placeholder="Password"
+            name="password"
+            onChange={(e) =>
+              setValues({ ...values, [e.target.name]: e.target.value })
+            }
+          />
+        </div>
+        <button type="submit">Submit</button>
+        <span>
+          Don't have an account ?<Link to="/register"> Register </Link>
+        </span>
+      </form>
+      <ToastContainer />
+  </>
+  );
+}
+
+export default Login;
---
+++ public/src/pages/Register.jsx
@@ -0,0 +1,93 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { ToastContainer, toast } from "react-toastify";
+import { useCookies } from "react-cookie";
+import { Link, useNavigate } from "react-router-dom";
+import { Button } from '@mui/material';
+import Toolbar from '@mui/material/Toolbar';
+import AppBar from '@mui/material/AppBar';
+
+
+
+function Register() {
+  const [cookies] = useCookies(["cookie-name"]);
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (cookies.jwt) {
+      navigate("/");
+    }
+  }, [cookies, navigate]);
+
+  const [values, setValues] = useState({ email: "", password: "" });
+  const generateError = (error) =>
+    toast.error(error, {
+      position: "bottom-right",
+    });
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const { data } = await axios.post(
+        "http://localhost:4000/register",
+        {
+          ...values,
+        },
+        { withCredentials: true }
+      );
+      if (data) {
+        if (data.errors) {
+          const { email, password } = data.errors;
+          if (email) generateError(email);
+          else if (password) generateError(password);
+        } else {
+          navigate("/");
+        }
+      }
+    } catch (ex) {
+      console.log(ex);
+    }
+  };
+  return (
+    <>
+    <AppBar position="static">
+          <Toolbar sx={{ justifyContent: "right" }} className='toolbar'>
+              <a href="/Register"><Button color="inherit">Sign Up</Button></a>
+              <a href="/Login"><Button color="inherit">Login</Button></a>
+          </Toolbar>
+      </AppBar>
+      <div className="App-header">Register to our application</div>
+      <p>Please fill out this form with the required informations</p>
+
+      <form onSubmit={(e) => handleSubmit(e)}>
+        <div>
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            name="email"
+            placeholder="Email"
+            onChange={(e) =>
+              setValues({ ...values, [e.target.name]: e.target.value })
+            }
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            placeholder="Password"
+            name="password"
+            onChange={(e) =>
+              setValues({ ...values, [e.target.name]: e.target.value })
+            }
+          />
+        </div>
+        <button type="submit">Submit</button>
+        <span>
+          Already have an account ?<Link to="/login"> Login</Link>
+        </span>
+      </form>
+      <ToastContainer />
+      </>
+  );
+}
+
+export default Register;
